bd7329dd07d98a6352e1a19db3b6e1b21b25eacd
Michal Apanowicz <michal.apanowicz@softwareone.com>
2025-01-22 15:18:10 +0100

    Config update

--- src/config/msalConfig.js
+++ src/config/msalConfig.js
@@ -3,11 +3,11 @@ export const msalConfig = {
     clientId: '13b6e063-87af-4aeb-a29e-b791121f904e', // This is the ONLY mandatory field that you need to supply.
     authority: 'https://apanowicz.b2clogin.com/apanowicz.onmicrosoft.com/b2c_1_susi', // Choose SUSI as your default authority.
     knownAuthorities: ['apanowicz.b2clogin.com'], // Mark your B2C tenant's domain as trusted.
-    redirectUri: 'https://b2c.apanowi.cz/', // You must register this URI on Azure Portal/App Registration. Defaults to window.location.origin
-    postLogoutRedirectUri: 'https://b2c.apanowi.cz/', // Indicates the page to navigate after logout.
+    redirectUri: 'https://b2c.apanowi.cz', // You must register this URI on Azure Portal/App Registration. Defaults to window.location.origin
+    postLogoutRedirectUri: 'https://b2c.apanowi.cz', // Indicates the page to navigate after logout.
   },
   cache: {
     cacheLocation: 'sessionStorage', // Configures cache location. 'sessionStorage' is more secure, but 'localStorage' gives you SSO between tabs.
-    storeAuthStateInCookie: true, // Set this to 'true' if you are having issues on IE11 or Edge
+    storeAuthStateInCookie: false, // Set this to 'true' if you are having issues on IE11 or Edge
   }
 }
